f958e1ea50a6c3829b533a50e76be4073c285428
digitalMoksha <brett@digitalmoksha.com>
2018-02-04 12:14:52 +0100

    make RubyMotion testing work again, and added a sample usage for the REPL

--- lib/motion-markdown-it-plugins/version.rb
+++ lib/motion-markdown-it-plugins/version.rb
@@ -1,5 +1,5 @@
 module MotionMarkdownItPlugins
 
-  VERSION = '1.0.0'
+  VERSION = '1.0.5'
 
 end
--- rubymotion/app/app_delegate.rb
+++ rubymotion/app/app_delegate.rb
@@ -3,12 +3,29 @@ class AppDelegate
   # OS X entry point
   #------------------------------------------------------------------------------
   def applicationDidFinishLaunching(notification)
+    usage
+    true
   end
 
   # iOS entry point
   #------------------------------------------------------------------------------
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    usage
     true
+  end
+
+  def usage
+    if RUBYMOTION_ENV == 'test'
+      puts '------------------------------------------------------------------------------'
+    else
+      puts '------------------------------------------------------------------------------'
+      puts 'Sample usage for simple testing in REPL:'
+      puts
+      puts 'md = MarkdownIt::Parser.new'
+      puts 'md.use(MotionMarkdownItPlugins::Ins)'
+      puts "md.render('++inserted++')"
+      puts '------------------------------------------------------------------------------'
+    end
   end
 
 end
